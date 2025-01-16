dc913845617252c83410f273c2cceba052bfbc3a
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-01-16 17:25:52 -0500

    oh frick

--- src/ProfilePage.cpp
+++ src/ProfilePage.cpp
@@ -5,18 +5,18 @@
 using namespace geode::prelude;
 
 class $modify(MyProfilePage, ProfilePage) {
-	void loadPageFromUserInfo(GJUserScore* p0) {
+	void setupCommentsBrowser(cocos2d::CCArray* p0) {
 		Manager* manager = Manager::getSharedInstance();
 		manager->doNotHighlight = true;
-		ProfilePage::loadPageFromUserInfo(p0);
+		ProfilePage::setupCommentsBrowser(p0);
 	}
 };
 
 class $modify(MyInfoLayer, InfoLayer) {
-	bool init(GJGameLevel *p0, GJUserScore *p1, GJLevelList *p2) {
+	void setupCommentsBrowser(cocos2d::CCArray* p0) {
 		Manager* manager = Manager::getSharedInstance();
-		if (p1) manager->doNotHighlight = true;
+		if (m_score) manager->doNotHighlight = true;
 		else manager->doNotHighlight = false;
-		InfoLayer::init(p0, p1, p2);
+		InfoLayer::setupCommentsBrowser(p0);
 	}
 };
