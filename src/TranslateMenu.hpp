3918bd9aa1bdd10bde9b7fc25b95e9560f9d8f9a
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-01-18 11:23:51 -0500

    v1.1.2 fakest

--- src/TranslateMenu.hpp
+++ src/TranslateMenu.hpp
@@ -7,7 +7,7 @@ protected:
 	void onCloseTranslateMenu(CCObject*);
 public:
 	static TranslateMenu* create(const std::string&);
-	static std::pair<std::string, std::string> TranslateMenu::findLanguageCodes(const std::unordered_map<std::string, std::string>& languagesMap, const bool isDeepL = false);
+	static std::pair<std::string, std::string> findLanguageCodes(const std::unordered_map<std::string, std::string>& languagesMap, const bool isDeepL = false);
 	static void encodeToURL(const std::string&);
 	void onBoogleTranslate(CCObject*);
 	void onLibreTranslate(CCObject*);
