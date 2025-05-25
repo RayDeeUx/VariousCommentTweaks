215329f8357f88f9655fd42724a19697946d2404
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-05-25 13:02:30 -0400

    oh for chrissake

--- src/Simpleton.cpp
+++ src/Simpleton.cpp
@@ -6,6 +6,7 @@ Simpleton* Simpleton::instance = nullptr;
 
 void Simpleton::getUserListFinished(cocos2d::CCArray* p0, UserListType p1) {
 	GameLevelManager::get()->m_userListDelegate = nullptr;
+	Manager* manager = Manager::getSharedInstance();
 	std::vector<int>& toInsert = p1 == UserListType::Blocked ? manager->ignoredUsers : manager->favoriteUsers;
 	std::vector<int>& toInsertTwo = p1 == UserListType::Blocked ? manager->blocked : manager->friends;
 	log::info("——— editing {} users using {} from GD ———", p1 == UserListType::Blocked ? "ignored users" : "favorite users", p1 == UserListType::Blocked ? "blocked list" : "friends list");
