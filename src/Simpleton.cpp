118663d08b553264acea5ae8202dadfb3715f873
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-05-25 12:59:36 -0400

    AHD

--- src/Simpleton.cpp
+++ src/Simpleton.cpp
@@ -8,11 +8,11 @@ void Simpleton::getUserListFinished(cocos2d::CCArray* p0, UserListType p1) {
 	GameLevelManager::get()->m_userListDelegate = nullptr;
 	std::vector<int>& toInsert = p1 == UserListType::Blocked ? manager->ignoredUsers : manager->favoriteUsers;
 	std::vector<int>& toInsertTwo = p1 == UserListType::Blocked ? manager->blocked : manager->friends;
-	log::info("——— editing {} users using {} from GD ———", p0 == UserListType::Blocked ? "ignored users" : "favorite users", p0 == UserListType::Blocked ? "blocked list" : "friends list");
+	log::info("——— editing {} users using {} from GD ———", p1 == UserListType::Blocked ? "ignored users" : "favorite users", p1 == UserListType::Blocked ? "blocked list" : "friends list");
 	for (GJUserScore* person : CCArrayExt<GJUserScore*>(p0)) {
 		const int id = person->m_accountID;
 		if (Utils::contains<int>(toInsert, id)) {
-			log::info("user ID {} was in existing vector in Manager class, skipping");
+			log::info("user ID {} was in existing vector in Manager class, skipping", id);
 			continue;
 		}
 		log::info("user ID {} was not in existing vector in Manager class, inserting it now", id);
