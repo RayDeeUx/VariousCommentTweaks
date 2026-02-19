a0eaf30e48b0a40c803a9fc5a4d9c5718cc9dee3
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-19 17:00:10 -0500

    boob

--- src/TranslateMenu.hpp
+++ src/TranslateMenu.hpp
@@ -2,13 +2,13 @@
 
 class TranslateMenu final : public geode::Popup<const std::string&> {
 protected:
-	void onOpenModSettings(CCObject*);
+	void onOpenModSettings(cocos2d::CCObject*);
 public:
 	static TranslateMenu* create(const std::string&);
 	bool setup(const std::string& text) override;
 	static std::pair<std::string, std::string> findLanguageCodes(const std::unordered_map<std::string, std::string>& languagesMap, const bool isDeepL = false);
 	static void encodeToURL(const std::string&);
-	void onBoogleTranslate(CCObject*);
-	void onLibreTranslate(CCObject*);
-	void onDeepLTranslate(CCObject*);
+	void onBoogleTranslate(cocos2d::CCObject*);
+	void onLibreTranslate(cocos2d::CCObject*);
+	void onDeepLTranslate(cocos2d::CCObject*);
 };
